0b168ad709779f5c0622b0be0086a42e457b75b7
Martin Feller <m.feller@auckland.ac.nz>
2016-01-15 13:23:33 +1300

    add annotation to avoid occasional errors during deployment on certain systems

--- src/main/java/nz/ac/auckland/eresearch/projectcentre/config/SecurityConfiguration.java
+++ src/main/java/nz/ac/auckland/eresearch/projectcentre/config/SecurityConfiguration.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,6 +29,7 @@ import javax.annotation.PostConstruct;
 @Configuration
 // NOTE: @EnableWebSecurity commented out because of https://github.com/spring-projects/spring-boot/issues/1548
 // @EnableWebSecurity
+@EnableGlobalAuthentication
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Autowired
